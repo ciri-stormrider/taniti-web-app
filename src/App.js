c65b06150cc760fd35b4b4380b4d673fd5de6dfc
Nirosha Lama <niroshalama@Mohits-MBP.attlocal.net>
2024-07-17 07:01:14 -0500

    banner msg for alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Divider } from "@mui/material";
+import { Alert, Divider } from "@mui/material";
 import Heading from "./Components/Heading/Heading";
 import SearchAppBar from "./Components/NavBar/SearchAppBar";
 import Content from "./Components/Content/Content";
 import Footer from "./Components/Footer/Footer";
+import { WarningAmberOutlined } from "@mui/icons-material";
 
 function App() {
   return (
     <>
       <SearchAppBar />
-      <Heading title="Taniti Village" subtitle="Adventure awaits you..."/>
+      <Alert icon={<WarningAmberOutlined fontSize="inherit" />} severity="warning">
+        Category 3 hurricane expected to hit shores of Yellow Leaf Bay on Friday July 18, 2024.
+        All emergency shelters are open to public. Your safety is out top priority. In case of emergency call 911.
+      </Alert>
+      <Heading title="Taniti Village" subtitle="Adventure awaits you..." />
       <Content />
       <Divider sx={{ margin: "1rem" }} />
       <Footer />
